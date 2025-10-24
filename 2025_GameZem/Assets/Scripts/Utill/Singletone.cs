64005c90e8b81872874235f76c0976dacb2f3753
EGURGINE <93648712+EGURGINE@users.noreply.github.com>
2025-10-25 01:18:33 +0900

    구조 정리
    - 메인씬, 타이틀 씬 추가
    - soundmanager, singletone 스크립트 추가

---
+++ 2025_GameZem/Assets/Scripts/Utill/Singletone.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    private static readonly object _lock = new object();
+    private static bool _applicationIsQuitting = false;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_applicationIsQuitting)
+            {
+                Debug.LogWarning($"[Singleton] Instance '{typeof(T)}' already destroyed. Returning null.");
+                return null;
+            }
+
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        GameObject singleton = new GameObject();
+                        _instance = singleton.AddComponent<T>();
+                        singleton.name = $"(Singleton) {typeof(T).Name}";
+
+                        DontDestroyOnLoad(singleton);
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void OnApplicationQuit()
+    {
+        _applicationIsQuitting = true;
+    }
+
+    protected virtual void OnDestroy()
+    {
+        _applicationIsQuitting = true;
+    }
+}
